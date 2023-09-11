79f21ffd5c927b950aab5b32816aa15de907ff71
groot85 <68751054+groot85@users.noreply.github.com>
2023-09-11 18:16:19 +0000

    exercicio resolvido, com comentarios

--- main.py
+++ main.py
@@ -3,20 +3,43 @@
 ## O programa abaixo deve calcular a média dos valores digitados pelo usuário.
 ## No entanto, ele não está funcionando bem. Você pode consertá-lo?
 
+# def calcular_media(valores):
+#     tamanho = 1
+#     soma = 0.0
+#     for i, valor in enumerate(valores):
+#         soma += valor
+#         i += 1
+#     media = soma / tamanho
+
+# continuar = True
+# valores = []
+# while continuar:
+#     valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor:')
+#     if valor.lower() == 'ok':
+#         continuar = false
+
+# media = calcular_media(valores)
+# print('A média calculada para os valores {} foi de {}'.format(valores, media))
+
+
 def calcular_media(valores):
-    tamanho = 1
+    tamanho = 0 #precisa começar com zero, para não dar erro na conta no final
     soma = 0.0
-    for i, valor in enumerate(valores):
+    for valor in valores: #modifivar para volor in valores
         soma += valor
-        i += 1
+        tamanho += 1 #definir que o meu tamanho é o total de itens que coloco na lista
     media = soma / tamanho
+    return media #colocar return da média
 
 continuar = True
 valores = []
 while continuar:
     valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor:')
     if valor.lower() == 'ok':
-        continuar = false
+        continuar = False #erro de escrita >>> False e não false
+    else:
+        valores.append(float (valor))
 
 media = calcular_media(valores)
+
 print('A média calculada para os valores {} foi de {}'.format(valores, media))
